91fcf257a8f3ee40cae86ea5eb7cc3ff74a63c17
Andreas Maurer <andreas.maurer@niceshops.com>
2020-04-25 09:54:40 +0200

    BeanInterface

---
+++ src/BeanInterface.php
@@ -0,0 +1,75 @@
+<?php
+declare(strict_types=1);
+/**
+ * @see       https://github.com/niceshops/nice-beans for the canonical source repository
+ * @license   https://github.com/niceshops/nice-beans/blob/master/LICENSE BSD 3-Clause License
+ */
+
+namespace NiceshopsDev\Bean;
+
+use Serializable;
+
+/**
+ * Interface BeanInterface
+ * @package Niceshops\Library\Core\Bean
+ */
+interface BeanInterface extends Serializable
+{
+    /**
+     * @param string $name
+     * @param mixed  $value
+     *
+     * @return BeanInterface
+     */
+    function setData($name, $value);
+    
+    
+    /**
+     * @param string $name
+     *
+     * @return mixed
+     */
+    function getData($name);
+    
+    
+    /**
+     * @param string $name
+     *
+     * @return bool
+     */
+    function hasData($name);
+    
+    
+    /**
+     * @param string $name
+     *
+     * @return mixed    the removed data or NULL if data couldn't be found
+     */
+    function removeData($name);
+    
+    
+    /**
+     * @return BeanInterface
+     */
+    function resetData();
+    
+    
+    /**
+     * @return array
+     */
+    function toArray();
+    
+    
+    /**
+     * @return array
+     */
+    function toArrayRecursive();
+    
+    
+    /**
+     * @param array $data
+     *
+     * @return mixed
+     */
+    function setFromArray(array $data);
+}
