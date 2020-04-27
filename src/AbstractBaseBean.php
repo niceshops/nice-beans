928aae92fb59d0fa8cc126d9010aa8779e41a294
Andreas Maurer <andreas.maurer@niceshops.com>
2020-04-27 19:24:20 +0200

    AbstractBaseBean - BeanInterface Implementation - setData

--- src/AbstractBaseBean.php
+++ src/AbstractBaseBean.php
@@ -22,4 +22,169 @@ abstract class AbstractBaseBean implements BeanInterface, ArrayAccess, IteratorA
 {
     
     use JsonSerializableTrait;
+    
+    
+    const DATA_TYPE_CALLABLE = 'callable';
+    
+    
+    /**
+     * @var array
+     */
+    private $data = [];
+    
+    
+    /**
+     * @var array   [ "<NORMALIZED_NAME>" => "<ORIGINAL_NAME>", ... ]
+     */
+    private $arrOriginalDataName = [];
+    
+    
+    /**
+     * @var array   [ "<NORMALIZED_NAME>" => "<DATA_TYPE" | <CALLABLE>, ... ]
+     */
+    private $arrDataType = [];
+    
+    
+    /**
+     * @param $name
+     *
+     * @return string
+     * @throws BeanException
+     */
+    public function normalizeDataName(string $name): string
+    {
+        if (is_string($name) && array_key_exists($name, $this->data)) {
+            return $name;
+        }
+        
+        $name = strtolower(trim($name));
+        
+        if (!strlen($name)) {
+            throw new BeanException("Invalid data name defined!");
+        }
+        
+        return $name;
+    }
+    
+    
+    /**
+     * @param string $originalName
+     * @param string $normalizedName
+     *
+     * @return $this
+     */
+    protected function setOriginalDataName(string $originalName, string $normalizedName)
+    {
+        $this->arrOriginalDataName[$normalizedName] = $originalName;
+        
+        return $this;
+    }
+    
+    
+    /**
+     * @param string $normalizedName
+     *
+     * @return mixed|string
+     */
+    protected function getOriginalDataName(string $normalizedName)
+    {
+        if (array_key_exists($normalizedName, $this->arrOriginalDataName)) {
+            return $this->arrOriginalDataName[$normalizedName];
+        }
+        
+        return $normalizedName;
+    }
+    
+    
+    /**
+     * @param string $normalizedName
+     *
+     * @return $this
+     */
+    protected function unsetOriginalDataName(string $normalizedName)
+    {
+        if (array_key_exists($normalizedName, $this->arrOriginalDataName)) {
+            unset($this->arrOriginalDataName[$normalizedName]);
+        }
+        
+        return $this;
+    }
+    
+    
+    /**
+     * @param string $name
+     *
+     * @return null|string  NULL if no datatype is defined for passed name
+     * @throws BeanException
+     */
+    protected function getDataType(string $name): ?string
+    {
+        $key = $this->normalizeDataName($name);
+        
+        if (isset($this->arrDataType[$key]) && is_string($this->arrDataType[$key])) {
+            return $this->arrDataType[$key];
+        }
+        
+        if (isset($this->arrDataType[$key]) && is_callable($this->arrDataType[$key])) {
+            return self::DATA_TYPE_CALLABLE;
+        }
+        
+        return null;
+    }
+    
+    
+    /**
+     * @param string $name
+     *
+     * @return callable|null
+     * @throws BeanException
+     */
+    protected function getDataTypeCallable(string $name): ?callable
+    {
+        $key = $this->normalizeDataName($name);
+        if (is_callable($this->arrDataType[$key])) {
+            return $this->arrDataType[$key];
+        }
+        
+        return null;
+    }
+    
+    
+    /**
+     * @param string $name
+     * @param        $value
+     *
+     * @return $this
+     * @throws BeanException     if invalid name is defined or data could not be set
+     */
+    public function setData($name, $value)
+    {
+        $origName = $name;
+        $name = $this->normalizeDataName($name);
+        if (is_null($name)) {
+            return $this;
+        }
+        
+        //  @todo isFrozen check at FreezableBeanTrait
+        
+        //  @todo isSealed check at SealableBeanTrait
+        
+        $error = false;
+        $arrName = null;
+        if (strpos($origName, ".") >= 1) {
+            $arrName = array_values(array_map("trim", explode(".", $origName)));
+            
+            $this->setOriginalDataName($arrName[0], $this->normalizeDataName($arrName[0]));
+        } else {
+            $this->setOriginalDataName($origName, $name);
+        }
+        
+        $dataType = $this->getDataType($name);
+        if ($dataType === self::DATA_TYPE_CALLABLE) {
+            $dataType = $this->getDataTypeCallable($name);
+        }
+        
+        
+        return $this;
+    }
 }
--- test/AbstractBaseBeanTest.php
+++ test/AbstractBaseBeanTest.php
@@ -54,4 +54,146 @@ class AbstractBaseBeanTest extends DefaultTestCase
         $this->assertTrue(is_a($this->object, AbstractBaseBean::class), "Mock Object is set");
     }
     
+    
+    /**
+     * @return array
+     */
+    public function normalizeDataNameDataProvider()
+    {
+        return [
+            ["foo", "foo", null],
+            ["Foo", "foo", null],
+            ["FOO", "foo", null],
+            [" foo ", "foo", null],
+            [" FOO ", "foo", null],
+            [" Foo ", "foo", null],
+            ["123", "123", null],
+            [123, "123", null],
+            ["FooBarBaz", "foobarbaz", null],
+            ["foo_bar_baz", "foo_bar_baz", null],
+            ["foo-bar-baz", "foo-bar-baz", null],
+            ["__foo__", "__foo__", null],
+            ["öäüßÖÄÜ", "öäüßÖÄÜ", null],
+            ["", "", BeanException::class],
+        ];
+    }
+    
+    
+    /**
+     * @group        unit
+     * @small
+     *
+     * @dataProvider normalizeDataNameDataProvider
+     *
+     * @covers       \NiceshopsDev\Bean\AbstractBaseBean::normalizeDataName()
+     *
+     * @param        $name
+     * @param string $expectedValue
+     * @param string $expectedException
+     */
+    public function testNormalizeDataName(string $name, string $expectedValue, string $expectedException = null)
+    {
+        if ($expectedException) {
+            $this->expectException($expectedException);
+        }
+        
+        $this->assertSame($expectedValue, $this->invokeMethod($this->object, "normalizeDataName", $name));
+    }
+    
+    
+    /**
+     * @group  unit
+     * @small
+     *
+     * @covers \NiceshopsDev\Bean\AbstractBaseBean::setOriginalDataName
+     * @covers \NiceshopsDev\Bean\AbstractBaseBean::getOriginalDataName
+     * @covers \NiceshopsDev\Bean\AbstractBaseBean::unsetOriginalDataName
+     */
+    public function testSetGetUnsetOriginalDataName()
+    {
+        
+        //  if no original name is set the method "getOriginalDataName" just return the passed name
+        $this->assertSame("Foo", $this->invokeMethod($this->object, "getOriginalDataName", ["Foo"]));
+        $this->assertSame("foo", $this->invokeMethod($this->object, "getOriginalDataName", ["foo"]));
+        
+        //  set original name
+        $this->invokeMethod($this->object, "setOriginalDataName", ["Foo", "foo"]);
+        $this->assertSame("Foo", $this->invokeMethod($this->object, "getOriginalDataName", ["Foo"]));
+        $this->assertSame("Foo", $this->invokeMethod($this->object, "getOriginalDataName", ["foo"]));
+        
+        //  try to unset none existing original name
+        $this->invokeMethod($this->object, "unsetOriginalDataName", ["Foo"]);
+        $this->assertSame("Foo", $this->invokeMethod($this->object, "getOriginalDataName", ["Foo"]));
+        $this->assertSame("Foo", $this->invokeMethod($this->object, "getOriginalDataName", ["foo"]));
+        
+        //  unset existing original name
+        $this->invokeMethod($this->object, "unsetOriginalDataName", ["foo"]);
+        $this->assertSame("Foo", $this->invokeMethod($this->object, "getOriginalDataName", ["Foo"]));
+        $this->assertSame("foo", $this->invokeMethod($this->object, "getOriginalDataName", ["foo"]));
+    }
+    
+    
+    /**
+     * @group  unit
+     * @small
+     *
+     * @covers \NiceshopsDev\Bean\AbstractBaseBean::getDataType
+     */
+    public function testGetDataType_isString()
+    {
+        $this->object = $this->getMockBuilder(AbstractBaseBean::class)->disableOriginalConstructor()->setMethods(
+            ["normalizeDataName"]
+        )->getMockForAbstractClass();
+        $name = " foo ";
+        $key = "foo";
+        $this->invokeSetProperty($this->object, "arrDataType", [$key => "bar"]);
+        
+        $this->object->expects($this->once())->method("normalizeDataName")->with(...[$name])->willReturn($key);
+        
+        $this->assertSame("bar", $this->invokeMethod($this->object, "getDataType", $name));
+    }
+    
+    
+    /**
+     * @group  unit
+     * @small
+     *
+     * @covers \NiceshopsDev\Bean\AbstractBaseBean::getDataType
+     */
+    public function testGetDataType_isCallable()
+    {
+        $this->object = $this->getMockBuilder(AbstractBaseBean::class)->disableOriginalConstructor()->setMethods(
+            ["normalizeDataName"]
+        )->getMockForAbstractClass();
+        $name = " foo ";
+        $key = "foo";
+        $callable = function () {
+        };
+        $this->invokeSetProperty($this->object, "arrDataType", [$key => $callable]);
+        
+        $this->object->expects($this->once())->method("normalizeDataName")->with(...[$name])->willReturn($key);
+        
+        $this->assertSame(AbstractBaseBean::DATA_TYPE_CALLABLE, $this->invokeMethod($this->object, "getDataType", $name));
+    }
+    
+    
+    /**
+     * @group  unit
+     * @small
+     *
+     * @covers \NiceshopsDev\Bean\AbstractBaseBean::getDataType
+     */
+    public function testGetDataType_isNull()
+    {
+        $this->object = $this->getMockBuilder(AbstractBaseBean::class)->disableOriginalConstructor()->setMethods(
+            ["normalizeDataName"]
+        )->getMockForAbstractClass();
+        $name = " foo ";
+        $key = "foo";
+        $this->invokeSetProperty($this->object, "arrDataType", []);
+        
+        $this->object->expects($this->once())->method("normalizeDataName")->with(...[$name])->willReturn($key);
+        
+        $this->assertNull($this->invokeMethod($this->object, "getDataType", $name));
+    }
 }
