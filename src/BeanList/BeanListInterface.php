dfa682a5cfdfbe0e5ee29a8734c8cef460e21f1b
Andreas Maurer <andreas.maurer@niceshops.com>
2020-04-27 21:42:57 +0200

    BeanListInterface

---
+++ src/BeanList/BeanListInterface.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+/**
+ * @see       https://github.com/niceshops/nice-beans for the canonical source repository
+ * @license   https://github.com/niceshops/nice-beans/blob/master/LICENSE BSD 3-Clause License
+ */
+
+namespace NiceshopsDev\Bean\BeanList;
+
+use NiceshopsDev\Bean\BeanInterface;
+
+interface BeanListInterface extends BeanInterface
+{
+    
+}
