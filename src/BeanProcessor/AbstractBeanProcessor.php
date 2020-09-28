6671ac6cf6ffeb924fba698f5cc3f0fa7257e34c
robertkleinschuster <robert.kleinschuster@niceshops.com>
2020-09-28 21:01:23 +0200

    Processor::validate

--- src/BeanProcessor/AbstractBeanProcessor.php
+++ src/BeanProcessor/AbstractBeanProcessor.php
@@ -81,7 +81,8 @@ abstract class AbstractBeanProcessor implements BeanProcessorInterface
         $isSaveAllowed = false;
         if ($this->hasOption(self::OPTION_SAVE_NON_EMPTY_ONLY) && $bean instanceof Countable) {
             $isSaveAllowed = $isSaveAllowed && $bean->count() > 0;
-        } elseif (!$this->hasOption(self::OPTION_IGNORE_VALIDATION)) {
+        }
+        if (!$this->hasOption(self::OPTION_IGNORE_VALIDATION)) {
             $isSaveAllowed = $isSaveAllowed && $this->validate($bean);
         } else {
             $isSaveAllowed = true;
