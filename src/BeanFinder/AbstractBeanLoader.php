a8269be2765fb298857dcfdf8a6c26eb7e2b8bba
Robert Kleinschuster <robert.kleinschuster@niceshops.com>
2020-10-11 15:19:37 +0200

    Update AbstractBeanLoader.php

--- src/BeanFinder/AbstractBeanLoader.php
+++ src/BeanFinder/AbstractBeanLoader.php
@@ -6,20 +6,5 @@ namespace NiceshopsDev\Bean\BeanFinder;
 
 abstract class AbstractBeanLoader implements BeanLoaderInterface
 {
-    public function current()
-    {
-        return $this->data();
-    }
-
-    public function next()
-    {
-        $this->fetch();
-    }
-
-    public function valid()
-    {
-        return $this->fetch();
-    }
-
-
+  
 }
